1b590101a80e4da61d3aed0b453b7463f54dcd2c
魏龙 <9131495+qinfeng_wei@user.noreply.gitee.com>
2024-06-10 17:54:16 +0800

    响应式登录页面

--- qf-admin/front/src/views/login/index.vue
+++ qf-admin/front/src/views/login/index.vue
@@ -1,180 +1,79 @@
 <template>
-    <div
-        style="
-            background-image: url('https://www.freeimg.cn/i/2024/01/03/6595660f7d875.jpg');
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        "
-    >
-        <div
-            style="
-                flex-direction: row;
-                display: flex;
-                border-radius: 60px;
-                width: 70%;
-                height: auto;
-            "
-        >
-            <div
-                style="
-                    background-color: hsl(39, 100%, 50%);
-                    width: 50%;
-                    height: auto;
-                    border-radius: 10px 0 0 10px;
-                    opacity: 0.3;
-                "
-            >
-                <div
-                    style="
-                        padding-left: 20px;
-                        padding-right: 10px;
-                        color: rgba(255, 255, 255, 0.947);
-                    "
-                >
-                    <span>
-                        <h1>欢迎登录</h1>
-                    </span>
-                    <div style="font-size: 18px">
-                        <span
-                            >&nbsp;&nbsp;&nbsp;清风后台管理平台,一个全栈攻城狮!</span
-                        >
-                        <br />
-                        <span
-                            >&nbsp;&nbsp;&nbsp;放弃该放弃的是无奈,放弃不该放弃的是无能,不放弃该放弃的是无知,不放弃不该放弃的是执着!</span
-                        >
+    <el-row style="min-height: 100vh;background-color: #509eee;">
+        <el-col :lg="16" :md="12" class="my-center">
+            <div style="font-size: 30px;font-weight: 900;">
+                欢迎访问清风管理后台
+            </div>
+
+        </el-col>
+        <el-col :lg="8" :md="12" class="my-center" style="background-color: white;">
+            <div style="width: 64%">
+                <div class="my-center" style="flex-direction: row;">
+                    <h2>qf-admin</h2>
+                    <div style="padding-left: 10px">
+                        <el-tag>1.0</el-tag>
                     </div>
                 </div>
-            </div>
-            <div
-                style="
-                    background-color: white;
-                    width: 50%;
-                    border-radius: 0 10px 10px 0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    padding-top: 10px;
-                    padding-bottom: 10px;
-                "
-            >
-                <div style="width: 80%">
-                    <div
-                        style="
-                            display: flex;
-                            flex-direction: row;
-                            justify-content: center;
-                            align-items: center;
-                        "
-                    >
-                        <h2>qf-admin</h2>
-                        <div style="padding-left: 10px">
-                            <el-tag>1.0</el-tag>
-                        </div>
-                    </div>
-                    <el-form
-                        ref="loginFormRef"
-                        :model="loginData"
-                        :rules="loginRules"
-                    >
-                        <el-form-item prop="username">
-                            <el-input
-                                ref="username"
-                                v-model="loginData.username"
-                                class="flex-1"
-                                size="large"
-                                placeholder="用户名"
-                                name="username"
-                            >
+                <el-form ref="loginFormRef" :model="loginData" :rules="loginRules">
+                    <el-form-item prop="username">
+                        <el-input ref="username" v-model="loginData.username" class="flex-1" size="large"
+                            placeholder="用户名" name="username">
+                            <template #prefix>
+                                <el-icon>
+                                    <i-ep-user />
+                                </el-icon>
+                            </template>
+                        </el-input>
+                    </el-form-item>
+
+                    <el-tooltip :disabled="isCapslock === false" content="大写锁定处于开启状态" placement="right">
+                        <el-form-item prop="password">
+                            <el-input v-model="loginData.password" placeholder="密码" :type="passwordVisible === false
+                                ? 'password'
+                                : 'input'
+                                " size="large" name="password" @keyup="checkCapslock" @keyup.enter="handleLogin">
                                 <template #prefix>
                                     <el-icon>
-                                        <i-ep-user />
-                                    </el-icon>
-                                </template>
-                            </el-input>
-                        </el-form-item>
-
-                        <el-tooltip
-                            :disabled="isCapslock === false"
-                            content="大写锁定处于开启状态"
-                            placement="right"
-                        >
-                            <el-form-item prop="password">
-                                <el-input
-                                    v-model="loginData.password"
-                                    placeholder="密码"
-                                    :type="
-                                        passwordVisible === false
-                                            ? 'password'
-                                            : 'input'
-                                    "
-                                    size="large"
-                                    name="password"
-                                    @keyup="checkCapslock"
-                                    @keyup.enter="handleLogin"
-                                >
-                                    <template #prefix>
-                                        <el-icon>
-                                            <i-ep-lock />
-                                        </el-icon>
-                                    </template>
-                                    <template #suffix>
-                                        <div @click="closeaaa">
-                                            <svg-icon
-                                                v-show="!passwordVisible"
-                                                icon-class="eye"
-                                            />
-                                            <svg-icon
-                                                v-show="passwordVisible"
-                                                icon-class="eye-open"
-                                            />
-                                        </div>
-                                    </template>
-                                </el-input>
-                            </el-form-item>
-                        </el-tooltip>
-
-                        <!-- 验证码 -->
-                        <el-form-item prop="captchaCode">
-                            <el-input
-                                v-model="loginData.captchaCode"
-                                auto-complete="off"
-                                placeholder="验证码"
-                                @keyup.enter="handleLogin"
-                            >
-                                <template #prefix>
-                                    <el-icon>
-                                        <svg-icon icon-class="captcha" />
+                                        <i-ep-lock />
                                     </el-icon>
                                 </template>
                                 <template #suffix>
-                                    <el-image
-                                        :src="captchaBase64"
-                                        @click="getCaptcha"
-                                        class="w-[100px] h-[40px]"
-                                    >
-                                        <template #error>
-                                            <el-icon size="20">
-                                                <i-ep-picture />
-                                            </el-icon>
-                                        </template>
-                                    </el-image>
+                                    <div @click="closeaaa">
+                                        <svg-icon v-show="!passwordVisible" icon-class="eye" />
+                                        <svg-icon v-show="passwordVisible" icon-class="eye-open" />
+                                    </div>
                                 </template>
                             </el-input>
                         </el-form-item>
-                        <el-button
-                            style="width: 100%"
-                            :loading="loading"
-                            type="primary"
-                            @click.prevent="handleLogin"
-                            >登录
-                        </el-button>
-                    </el-form>
-                </div>
+                    </el-tooltip>
+
+                    <!-- 验证码 -->
+                    <el-form-item prop="captchaCode">
+                        <el-input v-model="loginData.captchaCode" auto-complete="off" placeholder="验证码"
+                            @keyup.enter="handleLogin">
+                            <template #prefix>
+                                <el-icon>
+                                    <svg-icon icon-class="captcha" />
+                                </el-icon>
+                            </template>
+                            <template #suffix>
+                                <el-image :src="captchaBase64" @click="getCaptcha" class="w-[100px] h-[40px]">
+                                    <template #error>
+                                        <el-icon size="20">
+                                            <i-ep-picture />
+                                        </el-icon>
+                                    </template>
+                                </el-image>
+                            </template>
+                        </el-input>
+                    </el-form-item>
+                    <el-button style="width: 100%" :loading="loading" type="primary" @click.prevent="handleLogin">登录
+                    </el-button>
+                </el-form>
             </div>
-        </div>
-    </div>
+        </el-col>
+    </el-row>
+
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
@@ -300,5 +199,11 @@ onMounted(() => {
 <style lang="scss" scoped>
 .el-input {
     height: 50px;
+}
+
+.my-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
